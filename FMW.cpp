69c13eb6a6fc9d6193a8e9502bd5f4cdf84365b7
Roman Kholodov <92523145+chetverochka@users.noreply.github.com>
2025-05-02 00:53:47 +0300

    Add files via upload

---
+++ FMW.cpp
@@ -0,0 +1,200 @@
+#include "FMW.h"
+#include "cocos2d.h"
+
+FMW::AudioPlayer* FMW::AudioPlayer::m_instance = nullptr;
+
+FMW::AudioPlayer::AudioPlayer() {
+    m_idCounter = 0;
+
+    FMOD_RESULT fmResult = FMOD::System_Create(&m_fmSystem);
+    if (fmResult != FMOD_OK) {
+        CCLOG("FMOD error! (%d) %s", fmResult, FMOD_ErrorString(fmResult));
+    }
+
+    fmResult = m_fmSystem->init(512, FMOD_INIT_NORMAL, nullptr);
+    if (fmResult != FMOD_OK) {
+        CCLOG("FMOD init error! (%d) %s", fmResult, FMOD_ErrorString(fmResult));
+    }
+}
+
+std::string FMW::AudioPlayer::fmodSoundPath(std::string path)
+{
+    bool isAbsolutePath = cocos2d::FileUtils::getInstance()->isAbsolutePath(path);
+    if (!isAbsolutePath) {
+        // file:///android_asset/ + path wrapper for android
+        if (cocos2d::Application::getInstance()->getTargetPlatform() == cocos2d::ApplicationBase::Platform::Android) {
+            return "file:///android_asset/" + path;
+        }
+        else {
+            return cocos2d::FileUtils::getInstance()->getDefaultResourceRootPath() + path;
+        }
+    }
+    return path;
+}
+
+FMW::Sound* FMW::AudioPlayer::createSound(std::string songPath, FMOD_MODE fmMode) {
+    FMW::Sound* fmwSound = new FMW::Sound();
+    FMOD_RESULT fmResult = m_fmSystem->createSound(FMOD_SOUND_PATH(songPath), fmMode, nullptr, &fmwSound->m_fmSound);
+    if (fmResult != FMOD_OK) {
+        CCLOG("FMOD sound load error! (%d) %s", fmResult, FMOD_ErrorString(fmResult));
+        AX_SAFE_DELETE(fmwSound);
+    }
+    fmwSound->m_fmSystem = m_fmSystem;
+    fmwSound->m_id = m_idCounter;
+    m_idCounter++;
+    m_soundsMap[fmwSound->m_id] = fmwSound;
+    m_sounds.push_back(fmwSound);
+    return fmwSound;
+}
+
+void FMW::AudioPlayer::destroySound(FMW::Sound* target) {
+    for (int i = 0; i < m_sounds.size(); i++) {
+        if (m_sounds[i] == target) {
+            m_sounds.erase(m_sounds.begin() + i);
+        }
+    }
+    delete target;
+    //AX_SAFE_DELETE(target);
+}
+
+void FMW::AudioPlayer::update() {
+    if (m_fmSystem) m_fmSystem->update();
+}
+
+FMW::Sound::Sound() {
+    m_userPaused = m_enginePaused = m_isDSPFFTinitialised = false;
+}
+
+void FMW::Sound::play() {
+    FMOD_RESULT result = m_fmSystem->playSound(m_fmSound, nullptr, false, &m_fmChannel);
+    if (result != FMOD_OK) {
+        CCLOG("FMOD play error! (%d) %s", result, FMOD_ErrorString(result));
+
+    }
+    initDSPFFT(m_fmSystem);
+}
+
+void FMW::Sound::stop() {
+    m_fmChannel->stop();
+}
+
+void FMW::Sound::pause() {
+    if (!m_userPaused) {
+        m_userPaused = true;
+        m_fmChannel->setPaused(true);
+    }
+}
+
+void FMW::Sound::resume() {
+    if (m_userPaused || !m_enginePaused) {
+        m_userPaused = false;
+        if (!m_enginePaused) m_fmChannel->setPaused(false);
+    }
+}
+
+bool FMW::Sound::isPaused() {
+    return m_userPaused;
+}
+
+bool FMW::Sound::isEnginePaused() {
+    return m_enginePaused;
+}
+
+void FMW::Sound::setEnginePaused(bool value) {
+    if (m_enginePaused != value) {
+        m_enginePaused = value;
+
+        if (value) {
+            pause();
+        }
+        else {
+            resume();
+        }
+    }
+}
+
+void FMW::AudioPlayer::forcePause(FMW::Sound* target)
+{
+    target->setEnginePaused(true);
+}
+
+void FMW::AudioPlayer::forceResume(FMW::Sound* target)
+{
+    target->setEnginePaused(false);
+}
+
+void FMW::AudioPlayer::forcePauseAll() {
+    for (Sound* target : m_sounds)
+        forcePause(target);
+}
+
+void FMW::AudioPlayer::forceResumeAll() {
+    for (Sound* target : m_sounds)
+        forceResume(target);
+}
+
+void FMW::Sound::initDSPFFT(FMOD::System* system) {
+    bool successInit = true;
+    FMOD_RESULT result = system->createDSPByType(FMOD_DSP_TYPE_FFT, &m_fmDSPFFT);
+
+    if (result == FMOD_OK) {
+        
+        result = system->createDSPByType(FMOD_DSP_TYPE_FFT, &m_fmDSPFFT);
+        if (result == FMOD_OK)
+        {
+            m_fmChannel->addDSP(0, m_fmDSPFFT);
+        }
+        else
+        {
+            CCLOG("FMOD createDSP error! (%d) %s", result, FMOD_ErrorString(result));
+            successInit = false;
+        }
+    }
+    else
+    {
+        successInit = false;
+        CCLOG("FMOD createDSP error! (%d) %s", result, FMOD_ErrorString(result));
+    }
+    if (successInit) m_isDSPFFTinitialised = true;
+}
+
+std::vector<float> FMW::Sound::getFFTSpectrum(unsigned int channel) {
+    FMOD::DSP* fft = m_fmDSPFFT;
+    std::vector<float> spectrum = {};
+    if (fft && m_isDSPFFTinitialised)
+    {
+        FMOD_DSP_PARAMETER_FFT* fftData = nullptr;
+        FMOD_RESULT result = fft->getParameterData(FMOD_DSP_FFT_SPECTRUMDATA, (void**)&fftData, nullptr, nullptr, 0);
+        if (result == FMOD_OK && fftData && fftData->length > 0)
+        {
+            unsigned int binCount = fftData->length;
+            unsigned int maxChannelIndex = 31;
+            if (channel > maxChannelIndex) channel = maxChannelIndex;
+
+            for (int i = 0; i < binCount; ++i)
+            {
+                float magnitude = fftData->spectrum[channel][i];
+                spectrum.push_back(magnitude);
+            }
+        }
+    }
+
+    return spectrum;
+}
+
+float FMW::Sound::getAverageSpectrumAmplitude(unsigned int channel) {
+    std::vector<float> spectrum = getFFTSpectrum(channel);
+    if (!spectrum.empty())
+    {
+        float sum = 0.f;
+        for (float& n : spectrum) sum += n;
+        return (float)sum / (float)spectrum.size();
+    }
+    return 0.f;
+}
+
+void FMW::Sound::update() {
+    if (m_isDSPFFTinitialised) {
+
+    }
+}
---
+++ FMW.h
@@ -0,0 +1,86 @@
+#ifndef __FMW_H__
+#define __FMW_H__
+
+#include "cocos2d.h"
+#include "fmod.hpp"
+#include "fmod_errors.h"
+#include <map>
+#include <vector>
+// FMOD Wrapper (FMW)
+
+#define FMOD_SOUND_PATH(__PATH__) FMW::AudioPlayer::getInstance()->fmodSoundPath(__PATH__).c_str()
+
+namespace FMW {
+    class AudioPlayer;
+
+    class Sound {
+    protected:
+        FMOD::Channel* m_fmChannel;
+        FMOD::Sound* m_fmSound;
+        FMOD::System* m_fmSystem;
+        FMOD::DSP* m_fmDSPFFT;
+
+        bool m_userPaused;
+        bool m_enginePaused; // force pause
+        bool m_isDSPFFTinitialised;
+        int  m_id;
+        void setEnginePaused(bool value);
+        void update();
+    public:
+        Sound();
+        ~Sound() {
+            stop();
+            m_fmSound->release();
+        }
+        void initDSPFFT(FMOD::System* system);
+        void play();
+        void stop();
+        void pause();
+        void resume();
+        bool isPaused();
+        bool isEnginePaused();
+        void setVolume(float value) { m_fmChannel->setVolume(value); }
+        float getVolume() { float volume = 0.f; m_fmChannel->getVolume(&volume); return volume; }
+        void setPitch(float value) { m_fmChannel->setPitch(value); }
+        float getPitch() { float pitch; m_fmChannel->getPitch(&pitch); return pitch; }
+        std::vector<float> getFFTSpectrum(unsigned int channel = 0);
+        float getAverageSpectrumAmplitude(unsigned int channel = 0);
+        void setCurrentTime(float timeInSeconds) {
+            unsigned int ms = timeInSeconds * 1000.f;
+            m_fmChannel->setPosition(ms, FMOD_TIMEUNIT_MS);
+        }
+        float getCurrentTime() {
+            unsigned int ms = 0;
+            m_fmChannel->getPosition(&ms, FMOD_TIMEUNIT_MS);
+            return (float)ms / 1000.f;
+        }
+
+        friend class AudioPlayer;
+    };
+
+    class AudioPlayer {
+    protected:
+        static AudioPlayer* m_instance;
+        AudioPlayer();
+
+        FMOD::System* m_fmSystem;
+        int m_idCounter;
+        std::map<int, FMW::Sound*> m_soundsMap;
+        std::vector<FMW::Sound*> m_sounds;
+    public:
+        static AudioPlayer* getInstance() { if (!m_instance) m_instance = new AudioPlayer(); return m_instance; }
+        FMOD::System* getFMODSystem() { return m_fmSystem; }
+
+        std::string fmodSoundPath(std::string path);
+        FMW::Sound* createSound(std::string songPath, FMOD_MODE fmMode = FMOD_DEFAULT);
+        void destroySound(FMW::Sound* target);
+        void update();
+
+        void forcePause(FMW::Sound* target);
+        void forceResume(FMW::Sound* target);
+        void forcePauseAll();
+        void forceResumeAll();
+    };
+}
+
+#endif // !__FMW_H__
